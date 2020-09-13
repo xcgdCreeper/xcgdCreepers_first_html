3b9f82236d0260d8fd7d17c5533f76e71ad90d14
xcgdCreeper <xcgdCreeper@users.noreply.github.com>
2020-09-13 16:11:17 +0800

    去掉第二志愿，修复浮动元素不消失的bug，增加备案号

--- script/script.js
+++ script/script.js
@@ -59,7 +59,7 @@ function ScollPostion() {
             if (i < 7) {
                 doc1.style.height = document.getElementById(name[i + 1]).offsetTop - t + "px";
                 doc2.style.backgroundImage = "URL(" + imgs[i + 1] + ")";
-                doc2.style.height=document.documentElement.clientHeight+"px";
+                doc2.style.height = document.documentElement.clientHeight + "px";
             }
         }
     }
@@ -68,53 +68,6 @@ function ScollPostion() {
         doc1.style.height = document.getElementById(name[0]).offsetTop - t + "px";
         doc2.style.backgroundImage = "URL(" + imgs[0] + ")";
     }
-    /*
-    if (t < document.getElementById(name[0]).offsetTop) {
-        doc1.style.backgroundImage = "URL(images/bg.jpg)";
-        doc1.style.height = document.getElementById(name[0]).offsetTop - t + "px";
-        doc2.style.backgroundImage = "URL(" + imgs[0] + ")";
-    }
-    if (t > document.getElementById(name[0]).offsetTop) {
-        doc1.style.backgroundImage = "URL(" + imgs[0] + ")";
-        doc1.style.height = document.getElementById(name[1]).offsetTop - t + "px";
-        doc2.style.backgroundImage = "URL(" + imgs[1] + ")";
-    }
-    if (t > document.getElementById(name[1]).offsetTop) {
-        doc1.style.backgroundImage = "URL(" + imgs[1] + ")";
-        doc1.style.height = document.getElementById(name[2]).offsetTop - t + "px";
-        doc2.style.backgroundImage = "URL(" + imgs[2] + ")";
-    }
-    if (t > document.getElementById("wailian").offsetTop) {
-        doc1.style.backgroundImage = "URL(" + imgs[2] + ")";
-        doc1.style.height = document.getElementById("xinxuan").offsetTop - t + "px";
-        doc2.style.backgroundImage = "URL(" + imgs[3] + ")";
-    }
-    if (t > document.getElementById("xinxuan").offsetTop) {
-        doc1.style.backgroundImage = "URL(" + imgs[3] + ")";
-        doc1.style.height = document.getElementById("chuanmei").offsetTop - t + "px";
-        doc2.style.backgroundImage = "URL(" + imgs[4] + ")";
-    }
-    if (t > document.getElementById("chuanmei").offsetTop) {
-        doc1.style.backgroundImage = "URL(" + imgs[4] + ")";
-        doc1.style.height = document.getElementById("kepu").offsetTop - t + "px";
-        doc2.style.backgroundImage = "URL(" + imgs[5] + ")";
-    }
-    if (t > document.getElementById("kepu").offsetTop) {
-        doc1.style.backgroundImage = "URL(" + imgs[5] + ")";
-        doc1.style.height = document.getElementById("chuangye").offsetTop - t + "px";
-        doc2.style.backgroundImage = "URL(" + imgs[6] + ")";
-    }
-    if (t > document.getElementById("chuangye").offsetTop) {
-        doc1.style.backgroundImage = "URL(" + imgs[6] + ")";
-        doc1.style.height = document.getElementById("chuangxin").offsetTop - t + "px";
-        doc2.style.backgroundImage = "URL(" + imgs[7] + ")";
-    }
-    if (t > document.getElementById("chuangxin").offsetTop) {
-        doc1.style.backgroundImage = "URL(" + imgs[7] + ")";
-        doc1.style.height = document.getElementById("chuangxin").offsetTop - t + "px";
-        doc2.style.backgroundImage = "URL(" + imgs[7] + ")";
-    }
-    */
 }
 
 
@@ -125,12 +78,12 @@ function floatpic() {
     var pic = document.getElementById("join-img");
     pic.style.marginTop = (4 / 5) * y + "px";
     pic.style.marginLeft = (x - 200) + "px";
-    if ((document.getElementById("sign-up").offsetTop - document.documentElement.scrollTop) < y - 100) {
+    console.log(document.body.scrollTop);
+    if (((document.getElementById("sign-up").offsetTop - document.body.scrollTop) < y) || ((document.getElementById("sign-up").offsetTop - document.documentElement.scrollTop) < y)) {
         pic.style.display = "none";
-        //console.log("hidden!");
     }
-    else{
-        pic.style.display="block";
+    else {
+        pic.style.display = "block";
     }
 }
 
@@ -142,10 +95,10 @@ function hiddenElement() {
     var second_w = document.getElementById("second-wish").value;
     var label = [];
     label[0] = document.createElement("label");
-    label[1] = document.createElement("label");
+    //label[1] = document.createElement("label");
     var select = [];
     select[0] = document.createElement("select");
-    select[1] = document.createElement("select");
+    //select[1] = document.createElement("select");
 
     var Value = ["ruanjian", "yingjian", "weixiu", "jingmo", "hangmo"];
     var name = ["软件组", "硬件组", "义务维修组", "静模组", "航模组"];
@@ -156,42 +109,44 @@ function hiddenElement() {
         option[i].innerHTML = name[i];
         select[0].appendChild(option[i]);
     }
+    /*
     for (var i = 0; i < 5; i++) {
         option[i] = document.createElement("option");
         option[i].value = Value[i];
         option[i].innerHTML = name[i];
         select[1].appendChild(option[i]);
     }
+    */
     var td = [];
     td[0] = document.getElementById("kaifaTd1");
-    td[1] = document.getElementById("kaifaTd2");
+    //td[1] = document.getElementById("kaifaTd2");
     while (td[0].firstChild != null) {
         td[0].removeChild(td[0].firstChild);
-        td[1].removeChild(td[1].firstChild);
+        //td[1].removeChild(td[1].firstChild);
     }
 
     //如果选了开发部，则显示
     if (first_w == "kaiFa" && second_w == "kaiFa") {
         while (td[0].firstChild != null) {
             td[0].removeChild(td[0].firstChild);
-            td[1].removeChild(td[1].firstChild);
+            //td[1].removeChild(td[1].firstChild);
         }
     }
     else if (first_w == "kaiFa" || second_w == "kaiFa") {
-        label[0].innerHTML = "技术部第一志愿:";
-        label[1].innerHTML = "技术部第二志愿:";
+        label[0].innerHTML = "技术开发部志愿:";
+        //label[1].innerHTML = "技术部第二志愿:";
         //console.log(td[0].firstChild);
         if (td[0].firstChild == null) {
             td[0].appendChild(label[0]);
             td[0].appendChild(select[0]);
-            td[1].appendChild(label[1]);
-            td[1].appendChild(select[1]);
+            //[1].appendChild(label[1]);
+            //td[1].appendChild(select[1]);
         }
     }
     else {
         while (td[0].firstChild != null) {
             td[0].removeChild(td[0].firstChild);
-            td[1].removeChild(td[1].firstChild);
+            //td[1].removeChild(td[1].firstChild);
         }
     }
 }
